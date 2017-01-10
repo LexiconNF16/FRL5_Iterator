cc748fdd2c7de0dbe7b15c110643596f7137c902
Adrian Lozano <adrian@xenotype.com>
2017-01-10 11:55:44 +0100

    Add project files.

---
+++ Iterator/LimitedList.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace Iterator
+{
+    class LimitedList<T> : IEnumerable<T> where T : class
+    {
+        private T[] elements;
+
+        public int Capacity { get; }
+        public int Count { get; private set; }
+
+        public LimitedList(int capacity)
+        {
+            Capacity = capacity;
+            elements = new T[capacity];
+        }
+
+        public bool Add(T element)
+        {
+            if (Count >= Capacity) return false;
+
+            for (int i = 0; i < Capacity; i++)
+            {
+                if (elements[i] == null)
+                {
+                    elements[i] = element;
+                    Count += 1;
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        public T Remove(T element)
+        {
+            for (int i = 0; i < Capacity; i++)
+            {
+                if (elements[i].Equals(element))
+                {
+                    elements[i] = null;
+                    Count -= 1;
+                    return element;
+                }
+            }
+            return default(T);
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            for (int i = 0; i < Capacity; i++)
+            {
+                if (elements[i] != null)
+                {
+                    yield return elements[i];
+                }
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+}
---
+++ Iterator/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+//using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Iterator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Explicit loop
+            //var enumerator = Naturals().GetEnumerator();
+            //Console.WriteLine(enumerator.Current);
+            //while (enumerator.MoveNext()) {
+            //    Console.WriteLine(enumerator.Current);
+            //    Console.ReadLine();
+            //}
+
+            // LimitedList
+            var backPack = new LimitedList<string>(10);
+
+            bool added;
+            int count = 0;
+            do
+            {
+                count += 1;
+                added = backPack.Add($"item {count}");
+            } while (added);
+            Inventory(backPack);
+            Console.ReadKey(true);
+
+            count = 0;
+            string remove = null;
+            foreach (var item in backPack)
+            {
+                if (count == 3)
+                {
+                    remove = item;
+                }
+                count += 1;
+            }
+            backPack.Remove(remove);
+            Inventory(backPack);
+        }
+
+        private static void Inventory(LimitedList<string> backPack)
+        {
+            Console.WriteLine();
+            Console.WriteLine($"{backPack.Count} items:");
+            foreach (var item in backPack)
+            {
+                Console.WriteLine(item);
+            }
+        }
+
+        static IEnumerable<int> Naturals()
+        {
+            int i = 7;
+            while (true)
+            {
+                yield return i;
+                i += 1;
+            }
+        }
+    }
+}
